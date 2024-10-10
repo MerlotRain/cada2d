cd032db6fd2b90e14d79bf68012a1c48282d4061
MerlotRain <merlotrain@gmail.com>
2024-10-10 23:23:57 +0800

    init 2dcad_algorithm

---
+++ capi/cada2d_c.cc
@@ -0,0 +1,7 @@
+
+#include "shape.h"
+#include "vec2d.h"
+
+#define CADAShape cada::Shape
+
+#include "cada2d_capi.h"
---
+++ shape.h
@@ -0,0 +1,12 @@
+#ifndef CADA_SHAPE_H
+#define CADA_SHAPE_H
+
+namespace cada {
+
+
+
+class Shape {};
+
+} // namespace cada
+
+#endif
